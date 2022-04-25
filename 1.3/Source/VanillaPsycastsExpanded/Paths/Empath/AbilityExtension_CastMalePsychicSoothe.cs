14ea21a8bc1d3eab42c63c9d10621311a947c927
Taranchuk <nfhfyxer@gmail.com>
2022-04-26 02:48:35 +0300

    d

--- 1.3/Source/VanillaPsycastsExpanded/Paths/Empath/AbilityExtension_CastMalePsychicSoothe.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Empath/AbilityExtension_CastMalePsychicSoothe.cs
@@ -6,13 +6,14 @@ using Mono.Unix.Native;
     using Verse.Sound;
     using VFECore.Abilities;
     using Ability = VFECore.Abilities.Ability;
-	public class AbilityExtension_CastMalePsychicSoothe : AbilityExtension_AbilityMod
+	public class AbilityExtension_CastPsychicSoothe : AbilityExtension_AbilityMod
 	{
+		public Gender gender;
         public override void Cast(LocalTargetInfo target, Ability ability)
 {
 			foreach (Pawn pawn in ability.pawn.MapHeld.mapPawns.AllPawnsSpawned)
 			{
-				if (!pawn.Dead && pawn.gender == Gender.Male && pawn.needs != null && pawn.needs.mood != null)
+				if (!pawn.Dead && pawn.gender == gender && pawn.needs != null && pawn.needs.mood != null)
 				{
 					pawn.needs.mood.thoughts.memories.TryGainMemory((Thought_Memory)ThoughtMaker.MakeThought(ThoughtDefOf.ArtifactMoodBoost));
 				}
