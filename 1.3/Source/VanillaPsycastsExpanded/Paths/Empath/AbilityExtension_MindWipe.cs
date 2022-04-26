fae260dade79880f031e08173150ba7b1ac6965e
Taranchuk <nfhfyxer@gmail.com>
2022-04-27 01:07:41 +0300

    added the mind wipe code (not finished fully)

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Empath/AbilityExtension_MindWipe.cs
@@ -0,0 +1,24 @@
+﻿namespace VanillaPsycastsExpanded
+{
+    using RimWorld;
+using System.Security.Cryptography;
+    using Verse;
+using VFECore;
+    using VFECore.Abilities;
+    using Ability = VFECore.Abilities.Ability;
+    public class AbilityExtension_MindWipe : AbilityExtension_AbilityMod
+	{
+        public override void Cast(LocalTargetInfo target, Ability ability)
+        {
+            var pawn = target.Pawn;
+            if (pawn.Faction != ability.pawn.Faction)
+            {
+                pawn.SetFaction(ability.pawn.Faction);
+            }
+            pawn.needs.mood.thoughts.memories.Memories.Clear();
+            pawn.relations.ClearAllRelations();
+            pawn.skills = new Pawn_SkillTracker(pawn);
+            NonPublicMethods.GenerateSkills(pawn);
+        }
+    }
+}
