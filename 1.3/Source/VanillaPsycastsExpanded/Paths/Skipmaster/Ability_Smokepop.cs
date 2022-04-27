fa8cdccb1b4c4cdf3c6d104968574f039571e6b0
Joshua <legodude17@users.noreply.github.com>
2022-04-26 23:56:44 -0700

    Smokepop and Wallraise

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Skipmaster/Ability_Smokepop.cs
@@ -0,0 +1,16 @@
+﻿namespace VanillaPsycastsExpanded.Skipmaster
+{
+    using RimWorld;
+    using Verse;
+    using Ability = VFECore.Abilities.Ability;
+
+    public class Ability_Smokepop : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            GenExplosion.DoExplosion(target.Cell, this.pawn.MapHeld, this.GetRadiusForPawn(), DamageDefOf.Smoke, null, -1, -1f, null, null, null, null,
+                                     ThingDefOf.Gas_Smoke, 1f);
+        }
+    }
+}
---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Skipmaster/Ability_Wallraise.cs
@@ -0,0 +1,89 @@
+﻿namespace VanillaPsycastsExpanded.Skipmaster
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+    using VFECore.Abilities;
+    using Ability = VFECore.Abilities.Ability;
+
+    public class Ability_Wallraise : Ability
+    {
+        public AbilityExtension_Wallraise Props => this.def.GetModExtension<AbilityExtension_Wallraise>();
+
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            Map         map   = this.pawn.Map;
+            List<Thing> items = new();
+            items.AddRange(this.Props.AffectedCells(target, map).SelectMany(c => from t in c.GetThingList(map)
+                                                                                 where t.def.category == ThingCategory.Item
+                                                                                 select t));
+            foreach (Thing item in items) item.DeSpawn();
+
+            foreach (IntVec3 loc in this.Props.AffectedCells(target, map))
+            {
+                GenSpawn.Spawn(ThingDefOf.RaisedRocks, loc, map);
+                FleckMaker.ThrowDustPuffThick(loc.ToVector3Shifted(), map, Rand.Range(1.5f, 3f), CompAbilityEffect_Wallraise.DustColor);
+            }
+
+            foreach (Thing item in items)
+            {
+                IntVec3 cell = IntVec3.Invalid;
+                for (int i = 0; i < 9; i++)
+                {
+                    IntVec3 c = item.Position + GenRadial.RadialPattern[i];
+                    if (c.InBounds(map) && c.Walkable(map) && map.thingGrid.ThingsListAtFast(c).Count <= 0)
+                    {
+                        cell = c;
+                        break;
+                    }
+                }
+
+                if (cell != IntVec3.Invalid)
+                    GenSpawn.Spawn(item, cell, map);
+                else
+                    GenPlace.TryPlaceThing(item, item.Position, map, ThingPlaceMode.Near);
+            }
+        }
+
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            base.DrawHighlight(target);
+            GenDraw.DrawFieldEdges(this.Props.AffectedCells(target, this.pawn.Map).ToList(), this.ValidateTarget(target) ? Color.white : Color.red);
+        }
+
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = false)
+        {
+            if (this.Props.AffectedCells(target, this.pawn.Map).Any(c => c.Filled(this.pawn.Map)))
+            {
+                if (showMessages)
+                    Messages.Message("AbilityOccupiedCells".Translate(this.def.LabelCap), target.ToTargetInfo(this.pawn.Map),
+                                     MessageTypeDefOf.RejectInput, false);
+
+                return false;
+            }
+
+            if (this.Props.AffectedCells(target, this.pawn.Map).Any(c => !c.Standable(this.pawn.Map)))
+            {
+                if (showMessages)
+                    Messages.Message("AbilityUnwalkable".Translate(this.def.LabelCap), target.ToTargetInfo(this.pawn.Map),
+                                     MessageTypeDefOf.RejectInput, false);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class AbilityExtension_Wallraise : AbilityExtension_AbilityMod
+    {
+        public List<IntVec2> pattern;
+        public float         screenShakeIntensity;
+
+        internal IEnumerable<IntVec3> AffectedCells(LocalTargetInfo target, Map map) =>
+            this.pattern.Select(intVec => target.Cell + new IntVec3(intVec.x, 0, intVec.z)).Where(intVec2 => intVec2.InBounds(map));
+    }
+}
