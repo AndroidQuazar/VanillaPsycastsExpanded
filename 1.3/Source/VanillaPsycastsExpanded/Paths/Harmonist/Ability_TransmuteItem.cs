015c13312bf80ee0e0ed348d98c601fe4f4dce14
Joshua <legodude17@users.noreply.github.com>
2022-04-28 17:06:54 -0700

    Transmutation

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Harmonist/Ability_TransmuteItem.cs
@@ -0,0 +1,50 @@
+﻿namespace VanillaPsycastsExpanded.Harmonist
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+    using Verse;
+    using VFECore.Abilities;
+
+    public class Ability_TransmuteItem : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            Thing   item  = target.Thing;
+            float   value = item.MarketValue * item.stackCount;
+            IntVec3 pos   = item.Position;
+
+            List<ThingDef> allItems =
+                (from thingDef in DefDatabase<ThingDef>.AllDefs
+                 where thingDef.category == ThingCategory.Item
+                 where !thingDef.IsCorpse
+                 where !thingDef.MadeFromStuff
+                 where !thingDef.IsEgg
+                 let marketValue = thingDef.BaseMarketValue
+                 let count = Mathf.FloorToInt(value / thingDef.BaseMarketValue)
+                 where marketValue <= value
+                 where count       <= thingDef.stackLimit
+                 where count       >= 1
+                 select thingDef
+                ).ToList();
+
+            float WeightSelector(ThingDef def)
+            {
+                float amount = value / def.BaseMarketValue;
+                float weight = Mathf.Abs(amount - Mathf.FloorToInt(amount));
+                return weight;
+            }
+
+            float    maxWeight = allItems.Max(WeightSelector);
+            ThingDef chosen    = allItems.RandomElementByWeight(def => maxWeight - WeightSelector(def));
+            item.Destroy();
+            item            = ThingMaker.MakeThing(chosen);
+            item.stackCount = Mathf.FloorToInt(value / chosen.BaseMarketValue);
+            GenSpawn.Spawn(item, pos, this.pawn.Map);
+        }
+
+        public override bool CanHitTarget(LocalTargetInfo target) => this.targetParams.CanTarget(target.Thing, this) &&
+                                                                     GenSight.LineOfSight(this.pawn.Position, target.Cell, this.pawn.Map, true);
+    }
+}
