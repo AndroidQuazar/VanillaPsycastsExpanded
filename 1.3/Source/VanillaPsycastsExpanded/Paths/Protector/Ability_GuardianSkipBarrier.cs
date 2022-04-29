c208d48722a8628e9ba06659b8e953ca4a6ac916
Taranchuk <nfhfyxer@gmail.com>
2022-04-29 23:48:56 +0300

    added sounds and done guardian skip barrier ability

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Protector/Ability_GuardianSkipBarrier.cs
@@ -0,0 +1,37 @@
+﻿namespace VanillaPsycastsExpanded
+{
+    using RimWorld;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+using UnityEngine.Networking;
+    using Verse;
+    using Verse.AI;
+    using VFECore.Abilities;
+    using Ability = VFECore.Abilities.Ability;
+    public class Ability_GuardianSkipBarrier : Ability_HediffDuration
+    {
+        public override Gizmo GetGizmo()
+        {
+            var hediff = pawn.health.hediffSet.GetFirstHediffOfDef(VPE_DefOf.VPE_GuardianSkipBarrier);
+            if (hediff != null)
+            {
+                return new Command_Action
+                {
+                    defaultLabel = "VPE.CancelSkipbarrier".Translate(),
+                    defaultDesc = "VPE.CancelSkipbarrierDesc".Translate(),
+                    icon = this.def.icon,
+                    action = delegate
+                    {
+                        pawn.health.RemoveHediff(hediff);
+                    },
+                    order = 10f + (this.def.requiredHediff?.hediffDef?.index ?? 0) + (this.def.requiredHediff?.minimumLevel ?? 0)
+                };
+            }
+            else
+            {
+                return base.GetGizmo();
+            }
+        }
+    }
+}
