804bb85d68fd32a5d298213c8fa00766355b364c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-04-30 16:25:13 +0200

    Constructs initial

---
+++ 1.3/Source/VanillaPsycastsExpanded/DeathActionWorkers/DeathActionWorker_SlagChunk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaPsycastsExpanded.DeathActionWorkers
+{
+    class DeathActionWorker_SlagChunk
+    {
+    }
+}
