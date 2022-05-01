8b28ce12425cbf029eeb533df907e5a1f7eea311
Taranchuk <nfhfyxer@gmail.com>
2022-05-01 18:10:49 +0300

    done mass beckon

--- 1.3/Source/VanillaPsycastsExpanded/Paths/Warlord/AbilityExtension_ForceJobOnTarget.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Warlord/AbilityExtension_ForceJobOnTarget.cs
@@ -1,18 +1,22 @@
 ﻿namespace VanillaPsycastsExpanded
 {
     using RimWorld;
+    using System.Linq;
     using Verse;
     using Verse.AI;
     using VFECore.Abilities;
     using Ability = VFECore.Abilities.Ability;
-    public class AbilityExtension_ForceJobOnTarget : AbilityExtension_AbilityMod
+
+    public class AbilityExtension_ForceJobOnTargetBase : AbilityExtension_AbilityMod
     {
         public JobDef jobDef;
 
         public StatDef durationMultiplier;
-        public override void Cast(LocalTargetInfo target, Ability ability)
+
+        public FleckDef fleckOnTarget;
+
+        protected void ForceJob(LocalTargetInfo target, Ability ability)
         {
-            base.Cast(target, ability);
             Pawn pawn = target.Thing as Pawn;
             if (pawn != null)
             {
@@ -26,6 +30,31 @@
                 job.mote = MoteMaker.MakeThoughtBubble(pawn, ability.def.iconPath, maintain: true);
                 pawn.jobs.StopAll();
                 pawn.jobs.StartJob(job, JobCondition.InterruptForced);
+                if (fleckOnTarget != null)
+                {
+                    Ability.MakeStaticFleck(pawn.DrawPos, pawn.Map, fleckOnTarget, 1f, 0f);
+                }
+            }
+        }
+    }
+    public class AbilityExtension_ForceJobOnTarget : AbilityExtension_ForceJobOnTargetBase
+    {
+        public override void Cast(LocalTargetInfo target, Ability ability)
+        {
+            base.Cast(target, ability);
+            ForceJob(target, ability);
+        }
+    }
+
+    public class AbilityExtension_ForceJobOnTargetInRadius : AbilityExtension_ForceJobOnTargetBase
+    {
+        public override void Cast(LocalTargetInfo target, Ability ability)
+        {
+            base.Cast(target, ability);
+            foreach (var pawn in GenRadial.RadialDistinctThingsAround(target.Cell, ability.pawn.Map, ability.GetRadiusForPawn(), true)
+                .OfType<Pawn>().Where(x => x != ability.pawn))
+            {
+                ForceJob(pawn, ability);
             }
         }
     }
