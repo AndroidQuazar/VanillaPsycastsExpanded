a6f529333f5b28456c9d17214821e67f1231ee01
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-02 16:20:55 +0200

    Technomancer constructs

---
+++ 1.3/Source/VanillaPsycastsExpanded/Comps/CompProperties/CompProperties_SetStoneColour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
+
+namespace VanillaPsycastsExpanded
+{
+    public class CompProperties_SetStoneColour : CompProperties
+    {
+       
+      
+
+        public CompProperties_SetStoneColour()
+        {
+            this.compClass = typeof(CompSetStoneColour);
+        }
+    }
+}
---
+++ 1.3/Source/VanillaPsycastsExpanded/Comps/CompSetStoneColour.cs
@@ -0,0 +1,81 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using UnityEngine;
+using Verse;
+
+namespace VanillaPsycastsExpanded
+{
+    [StaticConstructorOnStartup]
+    public class CompSetStoneColour : ThingComp
+    {
+
+        private PawnRenderer pawn_renderer;
+      
+
+
+        public CompProperties_SetStoneColour Props
+        {
+            get
+            {
+                return (CompProperties_SetStoneColour)this.props;
+            }
+        }
+
+        
+
+
+        public void SetStoneColour(ThingDef thingDef)
+        {
+
+            
+                Pawn pawn = this.parent as Pawn;
+                if (this.pawn_renderer == null)
+                {
+                    this.pawn_renderer = pawn.Drawer.renderer;
+
+                }
+
+                Vector2 vector = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.drawSize;
+
+                
+                    LongEventHandler.ExecuteWhenFinished(delegate
+                    {
+                        if (this.pawn_renderer != null)
+                        {
+
+                            try
+                            {
+
+                                Color color = thingDef.graphic.color;
+                                Graphic_Multi nakedGraphic = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(pawn_renderer.graphics.nakedGraphic.path, ShaderDatabase.Cutout, vector, color);
+                                
+                                this.pawn_renderer.graphics.ResolveAllGraphics();
+                                this.pawn_renderer.graphics.nakedGraphic = nakedGraphic;
+                                (this.pawn_renderer.graphics.nakedGraphic.data = new GraphicData()).shadowData = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.shadowData;
+
+                            }
+                            catch (NullReferenceException) { }
+                        }
+
+                    });
+
+                
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+    }
+
