94c600c34ee86e99bf6432ba7370810196a0cd48
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-02 15:58:02 +0200

    Technomancer constructs

---
+++ 1.3/Source/VanillaPsycastsExpanded/Comps/CompBreakLink.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace VanillaPsycastsExpanded
+{
+
+
+    public class CompBreakLink : ThingComp, AnimalBehaviours.PawnGizmoProvider
+    {
+        public CompProperties_BreakLink Props => base.props as CompProperties_BreakLink;
+
+
+        public IEnumerable<Gizmo> GetGizmos()
+        {
+
+            
+
+                yield return new Command_Action
+                {
+                    defaultLabel = Props.gizmoLabel.Translate(),
+                    defaultDesc = Props.gizmoDesc.Translate(),
+                    icon = ContentFinder<Texture2D>.Get(Props.gizmoImage, true),
+                    action = delegate
+                    {
+                        this.parent.Kill(null);
+                    }
+                };
+            
+        }
+
+       
+    }
+
+
+}
---
+++ 1.3/Source/VanillaPsycastsExpanded/Comps/CompProperties/CompProperties_BreakLink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Verse.AI;
+
+namespace VanillaPsycastsExpanded
+{
+    public class CompProperties_BreakLink : CompProperties
+    {
+       
+        public string gizmoImage;
+        public string gizmoLabel;
+        public string gizmoDesc;
+
+        public CompProperties_BreakLink()
+        {
+            this.compClass = typeof(CompBreakLink);
+        }
+    }
+}
