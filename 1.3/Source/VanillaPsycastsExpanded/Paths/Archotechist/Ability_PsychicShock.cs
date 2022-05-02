0480982a1a0d97427202e27559bf1e9643b6a21c
Taranchuk <nfhfyxer@gmail.com>
2022-05-02 22:28:22 +0300

    done psychic shock

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Archotechist/Ability_PsychicShock.cs
@@ -0,0 +1,24 @@
+﻿namespace VanillaPsycastsExpanded
+{
+    using RimWorld;
+    using Verse;
+    using Verse.Sound;
+    using VFECore.Abilities;
+    public class Ability_PsychicShock : Ability_HediffDuration
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            if (Rand.Chance(0.3f))
+            {
+                FireUtility.TryAttachFire(target.Pawn, 0.5f);
+                BodyPartRecord brain = target.Pawn.health.hediffSet.GetBrain();
+                if (brain != null)
+                {
+                    int num = Rand.RangeInclusive(1, 5);
+                    target.Pawn.TakeDamage(new DamageInfo(DamageDefOf.Flame, num, 0f, -1f, this.pawn, brain));
+                }
+            }
+        }
+    }
+}
