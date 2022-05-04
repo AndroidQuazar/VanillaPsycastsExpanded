304c0ef9598700c2025627e2e03c323a5041d8b3
Joshua <legodude17@users.noreply.github.com>
2022-05-04 00:27:41 -0700

    Enchant Quality and Mend

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Technomancer/Ability_IncreaseQuality.cs
@@ -0,0 +1,38 @@
+﻿namespace VanillaPsycastsExpanded.Technomancer
+{
+    using RimWorld;
+    using Verse;
+    using Ability = VFECore.Abilities.Ability;
+
+    public class Ability_IncreaseQuality : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            CompQuality comp = target.Thing.TryGetComp<CompQuality>();
+            if (comp is not {Quality: < QualityCategory.Excellent}) return;
+            comp.SetQuality(comp.Quality + 1, ArtGenerationContext.Colony);
+            for (int i = 0; i < 16; i++) FleckMaker.ThrowMicroSparks(target.CenterVector3, this.pawn.Map);
+        }
+
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            if (!base.ValidateTarget(target, showMessages)) return false;
+
+            CompQuality comp;
+            if ((comp = target.Thing.TryGetComp<CompQuality>()) == null)
+            {
+                if (showMessages) Messages.Message("VPE.MustHaveQuality".Translate(), MessageTypeDefOf.RejectInput, false);
+                return false;
+            }
+
+            if (comp.Quality >= QualityCategory.Excellent)
+            {
+                if (showMessages) Messages.Message("VPE.QualityTooHigh".Translate(), MessageTypeDefOf.RejectInput, false);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Technomancer/Ability_Mend.cs
@@ -0,0 +1,19 @@
+﻿namespace VanillaPsycastsExpanded.Technomancer
+{
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+    using Ability = VFECore.Abilities.Ability;
+
+    public class Ability_Mend : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+            target.Thing.HitPoints = Mathf.Clamp(target.Thing.HitPoints + (int) ((this.pawn.GetStatValue(StatDefOf.PsychicSensitivity) - 0.9f) * 100f),
+                                                 target.Thing.HitPoints, target.Thing.MaxHitPoints);
+        }
+
+        public override bool CanHitTarget(LocalTargetInfo target) => base.CanHitTarget(target) && target.Thing is {def: {useHitPoints: true}};
+    }
+}
