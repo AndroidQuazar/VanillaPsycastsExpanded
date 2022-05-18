e18346d53da7924059f8eed2879a2e3e91654eba
Joshua <legodude17@users.noreply.github.com>
2022-05-18 12:37:10 -0700

    Location Swap

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Harmonist/Ability_LocationSwap.cs
@@ -0,0 +1,19 @@
+﻿namespace VanillaPsycastsExpanded.Harmonist
+{
+    using RimWorld.Planet;
+    using Skipmaster;
+
+    public class Ability_LocationSwap : Ability_Teleport
+    {
+        protected override void ModifyTargets(ref GlobalTargetInfo[] targets)
+        {
+            targets = new[]
+            {
+                targets[0],
+                new GlobalTargetInfo(this.pawn.Position, this.pawn.Map),
+                this.pawn,
+                new GlobalTargetInfo(targets[0].Cell, targets[0].Map)
+            };
+        }
+    }
+}
---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Harmonist/Ability_LuckTransfer.cs
@@ -0,0 +1,13 @@
+﻿namespace VanillaPsycastsExpanded.Harmonist
+{
+    using RimWorld.Planet;
+    using VFECore.Abilities;
+
+    public class Ability_LuckTransfer : Ability
+    {
+        public override void Cast(params GlobalTargetInfo[] targets)
+        {
+            base.Cast(targets);
+        }
+    }
+}
