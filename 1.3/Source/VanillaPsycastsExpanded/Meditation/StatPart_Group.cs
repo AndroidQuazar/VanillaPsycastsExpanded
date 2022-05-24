ebb3306351890eec0b3311b30b903511850eda5d
Simon Latusek <simon.latusek@gmail.com>
2022-05-24 17:35:47 +0200

    StatPart_Group added map check

--- 1.3/Source/VanillaPsycastsExpanded/Meditation/StatPart_Group.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Meditation/StatPart_Group.cs
@@ -10,23 +10,23 @@
         {
             if (!this.ApplyOn(req)) return;
 
-            val += GenRadial.RadialDistinctThingsAround(req.Thing.Position, req.Thing.Map, 5f, true).OfType<Pawn>()
-                            .Count(p => p.CurJobDef == JobDefOf.Meditate) switch
-            {
-                <=1 => 0,
-                2   => 0.06f,
-                3   => 0.2f,
-                4   => 0.45f,
-                >=5 => 0.8f
-            };
+            if (req.Thing.Map != null)
+                val += GenRadial.RadialDistinctThingsAround(req.Thing.Position, req.Thing.Map, 5f, true).OfType<Pawn>()
+                             .Count(p => p.CurJobDef == JobDefOf.Meditate) switch
+                {
+                    <=1 => 0,
+                    2   => 0.06f,
+                    3   => 0.2f,
+                    4   => 0.45f,
+                    >=5 => 0.8f
+                };
         }
 
         public override string ExplanationPart(StatRequest req)
         {
             if (!this.ApplyOn(req)) return "";
 
-            int num = GenRadial.RadialDistinctThingsAround(req.Thing.Position, req.Thing.Map, 5f, true).OfType<Pawn>()
-                               .Count(p => p.CurJobDef == JobDefOf.Meditate);
+            int num = req.Thing.Map != null ? GenRadial.RadialDistinctThingsAround(req.Thing.Position, req.Thing.Map, 5f, true).OfType<Pawn>().Count(p => p.CurJobDef == JobDefOf.Meditate) : 0;
             return "VPE.GroupFocus".Translate(num - 1) + ": " + this.parentStat.Worker.ValueToString(num switch
             {
                 <=1 => 0,
