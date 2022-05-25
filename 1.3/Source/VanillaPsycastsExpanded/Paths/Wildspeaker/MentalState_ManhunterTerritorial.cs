64bb249b263ce31c1ca268cf47b99c1ae449c002
Joshua <legodude17@users.noreply.github.com>
2022-05-24 21:32:06 -0700

    Animal Allies

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Wildspeaker/MentalState_ManhunterTerritorial.cs
@@ -0,0 +1,10 @@
+﻿namespace VanillaPsycastsExpanded.Wildspeaker
+{
+    using RimWorld;
+    using Verse.AI;
+
+    public class MentalState_ManhunterTerritorial : MentalState_Manhunter
+    {
+        public override bool ForceHostileTo(Faction f) => f.HostileTo(Faction.OfPlayer);
+    }
+}
