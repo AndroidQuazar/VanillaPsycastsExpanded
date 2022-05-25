26571e9dbb27ae2fd6d64cc3e60d3156eb965cbb
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-25 09:57:48 +0200

    Added skeleton

---
+++ 1.3/Source/VanillaPsycastsExpanded/MentalStates/MentalState_FriendlyManhunter.cs
@@ -0,0 +1,35 @@
+﻿// Verse.AI.MentalState_Manhunter
+using RimWorld;
+using Verse;
+using Verse.AI;
+namespace VanillaPsycastsExpanded
+{
+	public class MentalState_FriendlyManhunter : MentalState
+	{
+		
+
+		public override bool ForceHostileTo(Thing t)
+		{
+			
+			if (t.Faction != null && t.Faction != Faction.OfPlayer)
+			{
+				return ForceHostileTo(t.Faction);
+			}
+			return false;
+		}
+
+		public override bool ForceHostileTo(Faction f)
+		{
+			if (!f.def.humanlikeFaction)
+			{
+				return f == Faction.OfMechanoids;
+			}
+			return true;
+		}
+
+		public override RandomSocialMode SocialModeMax()
+		{
+			return RandomSocialMode.Off;
+		}
+	}
+}
