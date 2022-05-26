05c7626680d62b3b484af297844a9ad9f4ef7d38
Joshua <legodude17@users.noreply.github.com>
2022-05-26 14:15:44 -0700

    Manhunting Manhunters

--- 1.3/Source/VanillaPsycastsExpanded/Paths/Wildspeaker/MentalState_ManhunterTerritorial.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Wildspeaker/MentalState_ManhunterTerritorial.cs
@@ -1,10 +1,13 @@
 ﻿namespace VanillaPsycastsExpanded.Wildspeaker
 {
     using RimWorld;
+    using Verse;
     using Verse.AI;
 
     public class MentalState_ManhunterTerritorial : MentalState_Manhunter
     {
         public override bool ForceHostileTo(Faction f) => f.HostileTo(Faction.OfPlayer);
+
+        public override bool ForceHostileTo(Thing t) => t.HostileTo(Faction.OfPlayer);
     }
 }
