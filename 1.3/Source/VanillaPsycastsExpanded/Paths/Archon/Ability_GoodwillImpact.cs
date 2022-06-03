7342461c6317d5708c25fc17bf0b77e3d07afa8d
Taranchuk <nfhfyxer@gmail.com>
2022-06-03 13:20:30 +0300

    word of alliance

---
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Archon/Ability_GoodwillImpact.cs
@@ -0,0 +1,39 @@
+﻿namespace VanillaPsycastsExpanded
+{
+    using RimWorld;
+    using RimWorld.Planet;
+    using UnityEngine;
+    using Verse;
+    using Verse.Sound;
+    using VFECore.Abilities;
+    using Ability = VFECore.Abilities.Ability;
+    public class Ability_GoodwillImpact : Ability
+    {
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            if (target.Thing is Pawn targetPawn)
+            {
+                if (targetPawn.HostileTo(this.pawn) || targetPawn.Faction == this.pawn.Faction)
+                {
+                    if (showMessages)
+                    {
+                        Messages.Message("VPE.MustBeAllyOrNeutral".Translate(), targetPawn, MessageTypeDefOf.CautionInput);
+                    }
+                    return false;
+                }
+            }
+            return base.ValidateTarget(target, showMessages);
+        }
+
+        public override void Cast(params GlobalTargetInfo[] targets)
+        {
+            base.Cast(targets);
+            foreach (var target in targets)
+            {
+                var targetPawn = target.Thing as Pawn;
+                var goodwillImpact = (int)Mathf.Max(10f, (this.pawn.GetStatValue(StatDefOf.PsychicSensitivity) * 100) - 100);
+                targetPawn.Faction.TryAffectGoodwillWith(this.pawn.Faction, goodwillImpact);
+            }
+        }
+    }
+}
