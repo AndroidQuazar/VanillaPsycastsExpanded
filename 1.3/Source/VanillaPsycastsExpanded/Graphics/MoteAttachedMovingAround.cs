882134746265dd3497de2f3d2b9316ebc23b7703
Taranchuk <nfhfyxer@gmail.com>
2022-06-05 19:06:58 +0300

    paralysis pulse and corpse talk tweaks

--- 1.3/Source/VanillaPsycastsExpanded/Graphics/MoteAttachedMovingAround.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Graphics/MoteAttachedMovingAround.cs
@@ -1,24 +1,46 @@
 ﻿namespace VanillaPsycastsExpanded
 {
     using RimWorld;
+    using System;
     using UnityEngine;
     using Verse;
 
     public class MoteAttachedMovingAround : MoteAttached
     {
+        private float direction;
         private Vector3 curPosition;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            curPosition = new Vector3(Rand.Range(-0.5f, 0.5f), 0, Rand.Range(-0.5f, 0.5f));
+            exactPosition = GetRootPosition() + curPosition;
+            exactPosition.y = link1.Target.CenterVector3.y + 1;
+        }
         protected override void TimeInterval(float deltaTime)
         {
             base.TimeInterval(deltaTime);
             curPosition = GetNewMoveVector();
-            exactPosition = GetRootPosition() + curPosition;
+            var rootPosition = GetRootPosition();
+            exactPosition = rootPosition + curPosition;
+            exactPosition.y = link1.Target.CenterVector3.y + 1;
         }
 
         public Vector3 GetNewMoveVector()
         {
             var realPosition = new Vector2(curPosition.x, curPosition.z);
-            var newPosition = realPosition.Moved(Rand.Range(0, 360), 0.001f);
-            var pos = new Vector3(newPosition.x, exactPosition.y, newPosition.y);
+            direction += Rand.Range(-22.5f, 22.5f);
+            if (direction < -360)
+            {
+                direction = Mathf.Abs(direction - -360);
+            }
+            if (direction > 360)
+            {
+                direction = direction - 360;
+            }
+            var newPosition = realPosition.Moved(direction, 0.01f);
+            var pos = new Vector3(newPosition.x, 0, newPosition.y);
+            pos = Vector3.ClampMagnitude(pos, 0.5f);
             return pos;
         }
         public Vector3 GetRootPosition()
