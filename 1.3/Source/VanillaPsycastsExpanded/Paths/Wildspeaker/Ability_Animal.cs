30323d187703284f5ac7daeb0cc13a7cd606a92c
Joshua <legodude17@users.noreply.github.com>
2022-08-25 12:58:32 -0700

    Add goodWillImpact to Word of Animals

--- 1.3/Source/VanillaPsycastsExpanded/Paths/Wildspeaker/Ability_Animal.cs
+++ 1.3/Source/VanillaPsycastsExpanded/Paths/Wildspeaker/Ability_Animal.cs
@@ -1,24 +1,23 @@
-﻿namespace VanillaPsycastsExpanded.Wildspeaker
-{
-    using RimWorld;
-    using RimWorld.Planet;
-    using Verse;
-    using Ability = VFECore.Abilities.Ability;
+﻿namespace VanillaPsycastsExpanded.Wildspeaker;
 
-    public class Ability_Animal : Ability
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Ability = VFECore.Abilities.Ability;
+
+public class Ability_Animal : Ability
+{
+    public override void Cast(params GlobalTargetInfo[] targets)
     {
-        public override void Cast(params GlobalTargetInfo[] targets)
-        {
-            base.Cast(targets);
-            foreach (GlobalTargetInfo target in targets)
-                if (target.Thing is Pawn pawn)
-                    if (pawn.AnimalOrWildMan())
-                    {
-                        if (pawn.MentalStateDef == MentalStateDefOf.Manhunter || pawn.MentalStateDef == MentalStateDefOf.ManhunterPermanent)
-                            pawn.MentalState.RecoverFromState();
-                        else
-                            InteractionWorker_RecruitAttempt.DoRecruit(this.pawn, pawn);
-                    }
-        }
+        base.Cast(targets);
+        foreach (GlobalTargetInfo target in targets)
+            if (target.Thing is Pawn pawn)
+                if (pawn.AnimalOrWildMan())
+                {
+                    if (pawn.MentalStateDef == MentalStateDefOf.Manhunter || pawn.MentalStateDef == MentalStateDefOf.ManhunterPermanent)
+                        pawn.MentalState.RecoverFromState();
+                    else
+                        InteractionWorker_RecruitAttempt.DoRecruit(this.pawn, pawn);
+                }
     }
 }
